bd97e2da3b3ad5385f9832e0c31ea4fb175ebd25
SpacePistache <lukeholley@hotmail.com>
2024-10-21 12:02:10 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*hello I'm a commentary*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
