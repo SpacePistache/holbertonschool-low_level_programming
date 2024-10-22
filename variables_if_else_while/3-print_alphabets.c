2557e808299158f7897f650b36855e3160e887c2
SpacePistache <lukeholley@hotmail.com>
2024-10-22 12:58:22 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,17 +4,18 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
-	int al; 
+	int al;
+
 	for (al = 97; al > 123; al++)
 		putchar(al);
 
 
-	for (al = 65; al > 91; al ++)
+	for (al = 65; al > 91; al++)
 		putchar(al);
 
 	putchar('\n');
 
-	return (0); 
+	return (0);
 }
