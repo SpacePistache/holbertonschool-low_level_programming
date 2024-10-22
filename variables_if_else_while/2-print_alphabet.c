1a046b96cf75a6192f8cde83e1f2b073d8fe2b4f
SpacePistache <lukeholley@hotmail.com>
2024-10-22 12:08:09 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int al;
+
+	for (al = 65; al < 91; al++)
+		putchar(al);
+
+	putchar(' ');
+
+	return (0);
+}
