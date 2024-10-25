05c846054a0b6dccdb9f0b0f951dd86ed301f094
SpacePistache <lukeholley@hotmail.com>
2024-10-25 13:27:49 +0000

    4-isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_isalpha - checks is alphabetic character or not.
+ *@c: is a value I think.
+ *Return: 1 for alphabetic character, 0 for other.
  */
 int _isalpha(int c)
 {
-	if (c > 65 && < 123) 
+	if (c > 65 && c < 123)
 	{
-		return (1)
+		return (1);
 	}
 
 	else
