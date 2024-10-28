322939f674c886744126ffd835059a035b52ca12
SpacePistache <lukeholley@hotmail.com>
2024-10-28 14:54:01 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,17 @@ void more_numbers(void)
 {
 	int i, num;
 
-	for (i = 0; i > 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (num = 0; < 9; num++)		
-	
-	
-		_putchar(('0' + num / 10));
-		_putchar(('0' + num % 10));
-	}
-		_putchar(num);
+		for (num = 0; num <= 14; num++)
+		{
+			if (num  > 9)
+				_putchar('1');
+			_putchar('0' + num % 10);
+
+		}
 		_putchar('\n');
-		
+	}
+
 }
+
