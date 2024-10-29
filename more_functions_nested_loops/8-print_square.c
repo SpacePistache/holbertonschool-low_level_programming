d6621fb329c327a165910d3801923c64fb66f720
SpacePistache <lukeholley@hotmail.com>
2024-10-29 14:48:53 +0000

    8-squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,9 +8,9 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (j = 0; j <= size; j++)
+	for (j = 0; j < size; j++)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 			_putchar('#');
 		_putchar('\n');
 	}
