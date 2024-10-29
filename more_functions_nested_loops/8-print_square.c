1a3f79ab843784ef365a15c36bc7858cf9ef95a5
SpacePistache <lukeholley@hotmail.com>
2024-10-29 14:39:22 +0000

    8-squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,18 @@
 #include "main.h"
 /**
- *print_diagonal - print numbers except for two values.
- *@n: variable.
+ *print_square - print numbers except for two values.
+ *@size: variable.
  *Return: None, using void.
  */
 void print_square(int size)
 {
 	int i, j;
 
-	size = 2;
-
-	for (size = 2;  size++;)
-		_putchar('#');
-
-		for (i = 0; i > size; i++)
+	for (j = 0; j <= size; j++)
 	{
-		for (j = 0; j > i; j++)	
+		for (i = 0; i <= size; i++)
 			_putchar('#');
+		_putchar('\n');
 	}
-	_putchar('#');
-	_putchar('\n');
-	
 
 }
