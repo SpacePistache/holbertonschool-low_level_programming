02faf3d3d20c47472e9bfc2dbbe0b254d9df54e0
SpacePistache <lukeholley@hotmail.com>
2024-10-29 11:04:27 +0000

    7-diagonals

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_diagonal - print numbers except for two values.
+ *@n: variable.
+ *Return: None, using void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+
+	_putchar('\n');
+}
