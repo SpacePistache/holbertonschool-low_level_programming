88c87faa2d6008dfd13e239472c757beca3ecdf5
SpacePistache <lukeholley@hotmail.com>
2024-10-29 13:11:23 +0000

    7-diagonals

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,12 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(92);
-		_putchar('\n');
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
+		_putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
+
