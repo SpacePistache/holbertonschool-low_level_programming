6f09619ac38701f7ed2ccca6aba0d4b014d151db
SpacePistache <lukeholley@hotmail.com>
2024-10-31 11:15:37 +0000

    2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - swapping ints.
+ * @s: Is variable
+ *
+ * Return: 0 baby.
+ */
+int _strlen(char *s)
+{
+	int i; 
+	
+	while (s[i]!='\0')
+	{
+		i++;
+	}	
+
+	return (0);
+}
