75336d44d3d7ac14a51c32dd8017279ab77798f5
SpacePistache <lukeholley@hotmail.com>
2024-10-31 10:03:53 +0000

    1-swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *swap_ - swapping ints.
- *@temp: is temporary variable.
- *@a: Is variable
- *@b: is variable to be swapped.
- *Return: we voidin' today boys.
+ * swap_int - swapping ints.
+ * @a: Is variable
+ * @b: is variable to be swapped.
+ *
+ * Return: we voidin' today boys.
  */
 void swap_int(int *a, int *b)
 {
