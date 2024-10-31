6c44330aa48cc95938fa40ec073f55cf22c53802
SpacePistache <lukeholley@hotmail.com>
2024-10-31 09:36:27 +0000

    0-98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - changing the value addressed at x.
+ *@n: its the variable.
+ * Return: No need because we voidin' today
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
