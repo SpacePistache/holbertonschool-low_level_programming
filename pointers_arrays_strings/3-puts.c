52d9677f1900bea9e6329d535596c74b8dc6af3c
SpacePistache <lukeholley@hotmail.com>
2024-10-31 12:03:16 +0000

    3-puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - printing a string followed by a new line.
+ * @str: Is the string to print.
+ *
+ * Return: we voidin' today boys.
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+}
