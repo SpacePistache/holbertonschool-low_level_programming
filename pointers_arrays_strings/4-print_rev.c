7b1212531cdea54216d870bfdd2ba4841d29e795
SpacePistache <lukeholley@hotmail.com>
2024-10-31 14:36:20 +0000

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - swapping ints.
+ * @s: Is variable
+ *
+ * Return: we voidin' today boys.
+ */
+void print_rev(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+
+}
