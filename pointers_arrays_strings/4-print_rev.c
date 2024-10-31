3619f0fdc4ea15b942597315b2c9d11ce81c460b
SpacePistache <lukeholley@hotmail.com>
2024-10-31 15:07:58 +0000

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,14 @@
 void print_rev(char *s)
 {
 
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
+	while (s[i] != '\0')
+	       i++;	
+	
+	while (i - 1 >= 0)]
+		i--;
+		_putchar s[i];
 
 	_putchar('\n');
 
