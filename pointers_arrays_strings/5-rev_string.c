a91a9819b11709754348e6f1527998767717e2f2
SpacePistache <lukeholley@hotmail.com>
2024-11-02 11:38:18 +0000

    5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,25 @@
 #include "main.h"
 /**
- * _strlen - swapping ints.
+ * rev_string - reverse printing a string.
  * @s: Is variable
  *
- * Return: 0 baby.
+ * Return: We voidin' today boys.
  */
 void rev_string(char *s)
 {
-	
+	int length = 0;
+	int i;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
 }
