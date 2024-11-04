1913f5c7c01f6a9025733b7d13f8ef5b0c8f0628
SpacePistache <lukeholley@hotmail.com>
2024-11-04 20:41:03 +0000

    7-1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
  */
 char *leet(char *s)
 {
-        int i, j;
-        char letters[] = "aAeEoOtTlL";
-        char leet_chars[] = "4433007711";
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet_chars[] = "4433007711";
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; letters[j] != '\0'; j++)
-                {
-                        if (s[i] == letters[j])
-                        {
-                                s[i] = leet_chars[j];
-                                break;
-                        }
-                }
-        }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = leet_chars[j];
+				break;
+			}
+		}
+	}
 
-        return (s);
+	return (s);
 }
