6d7b2b6eec64fce7d79e101e9661bf95e8593e3b
SpacePistache <lukeholley@hotmail.com>
2024-11-05 07:16:10 +0000

    2-strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strchr - string character.
+ * @s: Is variable
+ * @c: doin things
+ * Return: returning nothing.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+
+	if (c == '\0')
+	{
+	return (s);
+	}
+
+	return (NULL);
+}
