0c6e50b2b8ff5d8ee00460d34699a47cd54cb13e
SpacePistache <lukeholley@hotmail.com>
2024-11-07 20:40:58 +0000

    2-strlen_recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion - printing in reverse characters.
+ * _strlen_recursion - printing in reverse characters.
  * @s: pointer.
  *
  * Return: We voidin' boys.
@@ -9,9 +9,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
-	_strlen_recursion(s);
-
-	return(s);
+	return (_strlen_recursion(s++) + 1);
 }
