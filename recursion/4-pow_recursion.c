21faaf58998631eb6048cc7d6c52d32416f8a309
SpacePistache <lukeholley@hotmail.com>
2024-11-08 13:51:04 +0000

    4-pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - value of x to the power of y.
+ * @x: is value to be calculated
+ * @y: value in the calculation
+ *
+ * Return: we're returning the value of x.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (x == 0)
+		return (0);
+	return (x * _pow_recursion(x, y - 1));
+}
