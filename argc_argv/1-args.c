bba007da600ca477eadb28f291605acd3ad7a87f
SpacePistache <lukeholley@hotmail.com>
2024-11-10 10:34:38 +0000

    nargs

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,14 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	(void) argv;
 
 	printf("argc = %d\n", argc);
-	
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
 
 	return (0);
 }
