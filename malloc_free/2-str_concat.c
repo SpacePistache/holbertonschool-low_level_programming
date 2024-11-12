1b9cc031b43de394c3711434934968a9a26d1cea
SpacePistache <lukeholley@hotmail.com>
2024-11-12 14:42:44 +0000

    2-str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - Concatenates the strings
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Concatenated string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int len1, len2;
+
+	if (s1 == NULL)
+		s1 = ("");
+	if (s2 == NULL)
+		s2 = ("");
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	concat = malloc(len1 + len2 + 1);
+
+	if (concat == NULL)
+		return (NULL);
+
+	strcpy(concat, s1);
+	strcat(concat, s2);
+
+	return (concat);
+}
