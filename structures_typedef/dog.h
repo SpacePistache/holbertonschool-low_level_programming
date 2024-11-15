3c69c400310fd467d0ac821da00a77d37b4d117c
SpacePistache <lukeholley@hotmail.com>
2024-11-15 18:06:09 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,6 +22,11 @@ struct dog *create_dog(char *name, float age, char *owner);
 void free_dog(struct dog *d);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 
 
 #endif
