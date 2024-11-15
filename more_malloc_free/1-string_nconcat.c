02548638517757549d1a4a698c29156c81fe9d0b
SpacePistache <lukeholley@hotmail.com>
2024-11-15 12:22:00 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * string_nconcat - Concatenate two strings.
  * @s1: First string
@@ -11,10 +10,17 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = (s1) ? strlen(s1) : 0;
-	unsigned int len2 = (s2) ? strlen(s2) : 0;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *result;
 
+	while (s1 && s1[len1])
+	{
+		len1++;
+	}
+	while (s2 && s2[len2])
+	{
+		len2++;
+	}
 	if (n > len2)
 	{
 		n = len2;
@@ -25,18 +31,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-
-	if (s1)
+	while (i < len1)
 	{
-		strcpy(result, s1);
+		result[i] = s1[i];
+		i++;
 	}
-
-	if (s2)
+	while (j < n)
 	{
-		strncpy(result + len1, s2, n);
+		result[i + j] = s2[j];
+		j++;
 	}
-
-	result[len1 + n] = '\0';
-
+	result[i + j] = '\0';
 	return (result);
 }
