1db9c9f7ea56c0e23ecf3ff4a9db4575a003a879
SpacePistache <lukeholley@hotmail.com>
2024-11-15 17:54:36 +0000

    b

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,6 +21,7 @@ struct dog
 struct dog *create_dog(char *name, float age, char *owner);
 void free_dog(struct dog *d);
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int is_prime_number(int n);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
