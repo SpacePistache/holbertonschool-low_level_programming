ce256be6ea587b735b3191df8d8e39db07a803f5
SpacePistache <lukeholley@hotmail.com>
2024-11-15 17:25:25 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: Always 0.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 struct dog *create_dog(char *name, float age, char *owner);
 void free_dog(struct dog *d);
 void print_dog(struct dog *d);
+
+
+#endif
