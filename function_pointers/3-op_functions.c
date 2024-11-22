9f7b8650e93079ce189934d283fd2d2e30973793
SpacePistache <lukeholley@hotmail.com>
2024-11-22 11:26:05 +0000

    finished calculator?

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> /* Required for exit() */
 #include "3-calc.h"
-
 /**
  * op_add - Returns the sum of a and b
  * @a: First integer
@@ -11,38 +10,64 @@
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
-
+/**
+ * op_sub - Returns the difference of a and b
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference of a and b
+ */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
-
+/**
+ * op_mul - Returns the product of a and b
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b
+ */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
-
+/**
+ * op_div - Returns the result of dividing a by b
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of division
+ */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a / b);
-}
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of dividing a by b
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder of division
+ */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
 }
