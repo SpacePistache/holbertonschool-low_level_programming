debbb13318bcd772cffef0c4b708b60c899b9092
SpacePistache <lukeholley@hotmail.com>
2024-11-22 11:09:33 +0000

    betty changes

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,25 +1,31 @@
 #include "3-calc.h"
 #include <stddef.h>
-
+/**
+ * op_add_func - function, return a and b
+ *
+ * return: result of a and b
+ */
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s && s[1] == '\0')
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
 
