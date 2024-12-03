105cab313088558d788adb91e4cc1372064a807b
SpacePistache <lukeholley@hotmail.com>
2024-12-03 14:48:16 +0100

    a

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,18 +18,6 @@ typedef struct list_s
 } list_t;
 
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 size_t print_list(const list_t *h);
 
 #endif
