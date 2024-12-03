cfcb9e545aeee65c713b0de40466026c55e7ba01
SpacePistache <lukeholley@hotmail.com>
2024-12-03 14:38:18 +0100

    0-print_list.c

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,35 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <unistd.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t print_list(const list_t *h);
+
+#endif
