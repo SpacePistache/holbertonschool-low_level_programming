aeb6570abbab47984431040f237934fcb45a31dc
SpacePistache <lukeholley@hotmail.com>
2024-12-04 09:57:56 +0100

    a betty format update

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t i = 0;
-    
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+size_t i = 0;
 
-        h = h->next;
-        i++;
-    }
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 
-    return i;
+h = h->next;
+i++;
+}
+
+return (i);
 }
