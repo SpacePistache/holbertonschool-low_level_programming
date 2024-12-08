977fc9f3b884e41499a2e5fe6f85eb07402e904d
SpacePistache <lukeholley@hotmail.com>
2024-12-08 16:08:45 +0100

    a

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t bytes_read, bytes_written, total_written = 0;
+char *buffer;
+
+if (!filename)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (!buffer)
+{
+close(fd);
+return (0);
+}
+
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+while (total_written < bytes_read)
+{
+bytes_written = _putchar(buffer[total_written]);
+if (bytes_written == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+total_written++;
+}
+
+free(buffer);
+close(fd);
+return (total_written);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,6 @@
 
 int _putchar(char c);
 
-int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
