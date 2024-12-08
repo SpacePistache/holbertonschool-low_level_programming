94a3952f21a302185ddf91fa23828a9336e5bb18
SpacePistache <lukeholley@hotmail.com>
2024-12-08 15:57:45 +0100

    c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: NULL-terminated string to append.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, len = 0, written_bytes;
+
+if (!filename)
+return (-1);
+
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+
+if (text_content)
+{
+while (text_content[len])
+len++;
+     
+written_bytes = write(fd, text_content, len);
+if (written_bytes == -1 || written_bytes != len)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
+}
